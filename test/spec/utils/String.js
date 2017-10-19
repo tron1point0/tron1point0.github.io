49c860445ae41ba89a7af9e618f28925ff2f4c0a
Tron <tron1point0@gmail.com>
2017-10-19 11:06:33 -0500

    Added string hashing to utilities

--- test/spec/utils/String.js
+++ test/spec/utils/String.js
@@ -1,3 +1,19 @@
 describe("String", function() {
+    beforeAll(function(done) {
+        require(["utils/String"], function() {
+            done();
+        });
+    });
 
+    it("hashes the empty string as 0", function() {
+        expect("".hashCode()).toBe(0);
+    });
+
+    it("hashes \"0\" as 48", function() {
+        expect("0".hashCode()).toBe(48);
+    });
+
+    it("hashes \"The quick brown fox jumps over the lazy dog\" as -609428141", function() {
+        expect("The quick brown fox jumps over the lazy dog".hashCode()).toBe(-609428141);
+    });
 });
